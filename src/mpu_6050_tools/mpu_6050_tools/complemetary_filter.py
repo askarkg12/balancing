b58cbb8e1ffe34e4b220e1eb9e779a273ae24b6a
Askar Sulaimanov <askar.sulaimanov@arctoris.com>
2022-04-07 10:28:38 +0100

    Added flake8 confige file and .py file now passes flake8 check

--- src/mpu_6050_tools/mpu_6050_tools/complemetary_filter.py
+++ src/mpu_6050_tools/mpu_6050_tools/complemetary_filter.py
@@ -1,26 +1,35 @@
+"""Read raw data from MPU6050 and publish complementary filter data."""
 import rclpy
 from rclpy.node import Node
 
-import sensor_msgs
+from sensor_msgs.msg import Imu
 
 
 class MPU6050ComplemetaryFilter(Node):
+    """TODO."""
+
     def __init__(self):
+        """TODO."""
         super().__init__("mpu6050_complementary_filter")
-        self.publisher_ = self.create_publisher(
-            msg.String, "/imu/data_complementary", 10
-        )
         self.subscription = self.create_subscription(
-            sensor_msgs.msg.Imu, "/imu/data_raw", self.listener_callback, 10
+            Imu, "/imu/data_raw", self.listener_callback, 10
         )
         self.subscription
+        self.angleX = 0
 
     def listener_callback(self, msg):
-        pass
+        """TODO."""
+        print("Got a msg")
 
 
-def main():
-    print("Hi from mpy_6050_tools.")
+def main(args=None):
+    """TODO."""
+    rclpy.init(args=args)
+    comp_filter = MPU6050ComplemetaryFilter()
+    rclpy.spin(comp_filter)
+
+    comp_filter.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
