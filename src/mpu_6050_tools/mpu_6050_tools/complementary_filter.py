19b540ac9feaf53090dda29abbcd7c91c933a28a
Askar Sulaimanov <askar.sulaimanov@arctoris.com>
2022-04-13 19:27:01 +0100

    Started complementary filter functions

--- src/mpu_6050_tools/mpu_6050_tools/complementary_filter.py
+++ src/mpu_6050_tools/mpu_6050_tools/complementary_filter.py
@@ -1,4 +1,6 @@
 """Read raw data from MPU6050 and publish complementary filter data."""
+import math
+
 import rclpy
 from rclpy.node import Node
 
@@ -15,11 +17,21 @@ class MPU6050ComplementaryFilter(Node):
             Imu, "/imu/data_raw", self.listener_callback, 10
         )
         self.subscription
-        self.angleX = 0
+        self.accel_angle_z = 0
+        self.gyro_angle_x = 0
 
     def listener_callback(self, msg):
         """TODO."""
-        print("Got a msg")
+        self.accel_angle_x = math.atan(
+            msg.linear_acceleration.y
+            / math.sqrt(msg.linear_acceleration.z**2 + msg.linear_acceleration.x**2)
+        )
+        self.accel_angle_y = math.atan(
+            -1
+            * msg.linear_acceleration.x
+            / math.sqrt(msg.linear_acceleration.y**2 + msg.linear_acceleration.z**2)
+        )
+        self.gyro_angle_x = self.gyro_angle_x + msg.angular_velocity.x * 2
 
 
 def main(args=None):
