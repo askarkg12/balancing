a34b1cabaa8dd1b31afe8b60ab97c8aedbb102e0
Askar Sulaimanov <askarkg12@github.com>
2022-04-14 01:53:14 +0100

    IMU is now 100Hz + trying to add option to change period

--- balancing_teensy/balancing_teensy.ino
+++ balancing_teensy/balancing_teensy.ino
@@ -5,6 +5,7 @@
 #include <rclc/executor.h>
 
 #include <sensor_msgs/msg/imu.h>
+#include <std_msgs/msg/int64.h>
 //#include <geometry_msgs/msg/Quaternion.h>
 
 #include <Adafruit_MPU6050.h>
@@ -13,6 +14,7 @@
 
 Adafruit_MPU6050 mpu;
 
+rcl_subscription_t subscriber;
 rcl_publisher_t publisher;
 rclc_executor_t executor;
 rclc_support_t support;
@@ -21,6 +23,7 @@ rcl_node_t node;
 rcl_timer_t timer;
 
 sensor_msgs__msg__Imu imu_msg;
+std_msgs__msg__Int64 period_msg;
 sensors_event_t a, g, temp;
 //geometry_msgs__msg__Quaternion orientation;
 
@@ -37,8 +40,19 @@ void error_loop(){
 }
 
 void loop() {
-  delay(100);
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(1)));
+}
+
+void period_change_callback(const void * msgin)
+{  
+  const std_msgs__msg__Int64 * period_msg = (const std_msgs__msg__Int64 *)msgin;
+  int64_t old_period;
+  int64_t new_period = period_msg->data;
+  RCCHECK(rcl_timer_exchange_period(
+    &timer, 
+    RCL_MS_TO_NS(new_period),
+    &old_period
+    ));
 }
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
@@ -71,7 +85,13 @@ void setup() {
 
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-  RCCHECK(rclc_node_init_default(&node, "balancing_teensy", "", &support));
+  RCCHECK(rclc_node_init_default(&node, "balancing", "", &support));
+
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int64),
+    "/balancing/imu/period"));
 
   RCCHECK(rclc_publisher_init_default(
     &publisher,
@@ -79,7 +99,7 @@ void setup() {
     ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Imu),
     "/balancing/imu/data_raw"));
     
-  const unsigned int timer_timeout = 100;
+  const unsigned int timer_timeout = 10;
   RCCHECK(rclc_timer_init_default(
     &timer,
     &support,
@@ -88,6 +108,7 @@ void setup() {
 
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &period_msg, &period_change_callback, ON_NEW_DATA));
 
   if (!mpu.begin())
   {
